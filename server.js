638328b6081e79adc4c1da09875ef1455241ae29
Shivam-BugHunter <shiwamishra2005@gmail.com>
2026-02-27 11:27:48 +0530

    server_working

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 import express from "express";
+import { createServer } from 'http';
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+import { dirname,join } from "path";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server)
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req,res)=>{
+    console.log("GET request /");
+    res.sendFile(join(__dirname + "/app/index.html"));
+})
+
+io.on("connection",(socket)=>{
+    console.log(`Someone connected to socket server and socket id is ${socket.id}`);
+});
 
 app.listen(9000,()=>{
     console.log(`Server listening on Port 9000`);
